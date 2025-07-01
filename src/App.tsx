60f805b9463c4dd954882ed5b34bd89f1ae2ba1f
dubious-sol <dubious.solx@gmail.com>
2025-07-01 22:19:02 +0100

    Fix sidebar position and scrollable content

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
       <div className="min-h-screen bg-black flex">
         <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
         
-        <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex flex-col overflow-hidden ml-64">
           <Header {...getHeaderProps()} />
           
           <main className="flex-1 overflow-y-auto">
